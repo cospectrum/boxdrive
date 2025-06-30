ea73954eb484943c10677cc0e89f6fcc7f01d682
Alex Severin <severinalexeyv@gmail.com>
2025-06-30 17:50:00 +0300

    create schemas folder

---
+++ src/boxdrive/schemas/__init__.py
@@ -0,0 +1,3 @@
+from .store import BucketMetadata, BucketName, ContentType, ETag, Key, MaxKeys, Object, ObjectMetadata
+
+__all__ = ["BucketName", "Key", "ETag", "ContentType", "MaxKeys", "BucketMetadata", "ObjectMetadata", "Object"]
